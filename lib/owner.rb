7a59941dda7e0272321f77cd698fc9d0cfedd8ac
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-24 21:33:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,6 @@
 class Owner
   # code goes here
+<<<<<<< HEAD
   attr_accessor :name, :pets
   attr_reader :species
   
@@ -9,6 +10,17 @@ class Owner
     @species = species
     @name = name
     @pets = {fishes: [], cats: [], dogs: []}
+=======
+  attr_accessor :name, :pets, :fish, :dog, :cat
+  attr_reader :species
+  
+  @@all = []
+  @@pets = {fishes: [], cats: [], dogs: []}
+  
+  def initialize(species)
+    @species = species
+    @name = name
+>>>>>>> 0926ebc3a07be5cfc06ffa3d41f25827ba1d8622
     @@all << self
   end
   
@@ -27,6 +39,7 @@ class Owner
   def say_species
     "I am a #{species}."
   end
+<<<<<<< HEAD
   
  
   def buy_fish(fish)
@@ -43,18 +56,44 @@ class Owner
   
   def walk_dogs
     @pets[:dogs].each do |dog|
+=======
+
+  
+  def buy_fish(fish)
+    @@pets[:fishes] << Fish.new(fish)
+  end
+  
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+  
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+  
+  def walk_dogs
+    @@pets[:dogs].each do |dog|
+>>>>>>> 0926ebc3a07be5cfc06ffa3d41f25827ba1d8622
       dog.mood = "happy"
     end
   end
   
   def play_with_cats
+<<<<<<< HEAD
     @pets[:cats].each do |cat|
+=======
+    @@pets[:cats].each do |cat|
+>>>>>>> 0926ebc3a07be5cfc06ffa3d41f25827ba1d8622
       cat.mood = 'happy'
     end
   end
 
   def feed_fish
+<<<<<<< HEAD
     @pets[:fishes].each do |fish|
+=======
+    @@pets[:fishes].each do |fish|
+>>>>>>> 0926ebc3a07be5cfc06ffa3d41f25827ba1d8622
       fish.mood = 'happy'
     end
   end
